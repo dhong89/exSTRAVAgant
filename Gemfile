4dada56d98d799ada9ee628860a3c7a919b07a13
David Hong <davidhong_89@yahoo.com>
2020-04-06 20:57:51 -0400

    Added Users table and controllers

--- Gemfile
+++ Gemfile
@@ -23,11 +23,12 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
-
+  gem 'jquery-rails'
+  gem 'pry-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
@@ -48,10 +49,9 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'better_errors'
   gem 'binding_of_caller'
-  gem 'pry-rails'
   gem 'annotate'
   gem 'bcrypt'
-  gem 'jquery-rails'
+
 end
 
 group :test do
---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+
+validates :password_digest, :session_token, presence: true
+validates :username, :session_token, uniqueness: true
+validates :password, length: {minimum: 6 }, allow_nil: true
+after_initialize :ensure_session_token
+attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil unless user && user.is_password?(password)
+        user 
+    end
+
+    def is_password?(password)
+        bcrypy_password = BCrypt::Password.new(self.password_digest)
+        bcrypy_password.is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)    
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    def reset_session_token!
+        self.update!(session_token: self.class.generate_session_token)
+        self.session_token
+    end
+
+end

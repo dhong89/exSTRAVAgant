cd74fbd757b6301ef48eeb9a548c6e4a4d926d33
David Hong <davidhong_89@yahoo.com>
2020-04-06 21:14:06 -0400

    added backend auth

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::UsersController < ApplicationController
+skip_before_action :verify_authenticity_token
+
+    def create
+        @user = User.new(users_params)
+        if @user.save 
+            log_in(@user)
+            render 'api/users/show'
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+
+    end
+
+
+    def users_params 
+        params.require(:user).permit(:username, :password)
+    end
+end

986ab30f7df90f138ee125884b78cd0045b50d72
David Hong <davidhong_89@yahoo.com>
2020-11-04 13:48:59 -0500

    Re establish db and migrations on new mac

--- db/schema.rb
+++ db/schema.rb
@@ -15,27 +15,6 @@ ActiveRecord::Schema.define(version: 2020_04_17_015251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
   create_table "routes", force: :cascade do |t|
     t.string "route_name", null: false
     t.float "distance", null: false
@@ -78,5 +57,4 @@ ActiveRecord::Schema.define(version: 2020_04_17_015251) do
     t.integer "user_id"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
